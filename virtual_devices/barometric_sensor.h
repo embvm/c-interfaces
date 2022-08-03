e560a94d5edead591f4e9793886e1eaf02def8d4
Phillip Johnston <phillip@embeddedartistry.com>
2022-08-03 09:08:51 -0700

    Add copyright header and basic include

---
+++ virtual_devices/barometric_sensor.h
@@ -0,0 +1,4 @@
+// SPDX-FileCopyrightText: © 2022 Embedded Artistry LLC <contact@embeddedartistry.com>
+// SPDX-License-Identifier: MIT
+
+#include <stdint.h>
---
+++ virtual_devices/temperature_sensor.h
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: © 2022 Embedded Artistry LLC <contact@embeddedartistry.com>
+// SPDX-License-Identifier: MIT
+
+#include <stdint.h>
+
