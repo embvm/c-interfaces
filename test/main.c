bb7710bd5dba7d68ddfc7ac1f6b7624ad450e5e8
Phillip Johnston <phillip@embeddedartistry.com>
2022-08-04 13:33:52 -0700

    main.c: expand headers

--- test/main.c
+++ test/main.c
@@ -2,6 +2,8 @@
 *  This file is used to sanity check the syntax of each of the interfaces.
 */
 #include <virtual_devices/barometric_sensor.h>
+#include <virtual_devices/barometric_altimeter.h>
+#include <virtual_devices/barometric_pressure_sensor.h>
 #include <virtual_devices/humidity_sensor.h>
 #include <virtual_devices/temperature_sensor.h>
 
